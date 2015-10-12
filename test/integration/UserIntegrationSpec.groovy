c13d022a90083c6b0d202582bc55d93c650329a9
Damon <damonhayhurst@gmail.com>
2015-10-12 11:29:27 +0100

    tests

--- test/integration/UserIntegrationSpec.groovy
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Damon on 12/10/2015.
- */
-class UserIntegrationSpec {
-}
---
+++ test/integration/urlshortener/UserIntegrationSpec.groovy
@@ -0,0 +1,24 @@
+package urlshortener
+/**
+ * Created by Damon on 12/10/2015.
+ */
+
+import spock.lang.*
+class UserIntegrationSpec {
+
+    def "Save a user to the database"() {
+
+        given: "A brand new user"
+        def user = new User(username: 'user', password: 'password')
+
+        when: "The user is saved"
+        user.save()
+
+        then: "It saved successfully and can be added to the database"
+        user.errors.errorCount == 0
+        user.id != null
+        User.get(user.id).username == user.username
+
+    }
+
+}
